a7252cfc3895262d19f6666536302af6c9dfc4bf
unknown <kacperwoloszyn4@gmail.com>
2019-01-07 13:52:23 +0000

    Log in view/presenter

--- app/src/main/java/org/wit/emergencyescape/views/login/LoginPresenter.kt
+++ app/src/main/java/org/wit/emergencyescape/views/login/LoginPresenter.kt
@@ -1,2 +1,49 @@
 package org.wit.emergencyescape.views.login
 
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.toast
+import org.wit.emergencyescape.models.LocationFireStore
+import org.wit.emergencyescape.views.BasePresenter
+import org.wit.emergencyescape.views.BaseView
+import org.wit.emergencyescape.views.VIEW
+
+class LoginPresenter(view: BaseView) : BasePresenter(view), AnkoLogger {
+
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun doLogin(email: String, password: String) {
+        val showProgress: Any = view?.showProgress()!!
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
+            if (task.isSuccessful) {
+                view?.toast("login as " + FirebaseAuth.getInstance().currentUser?.email.toString())
+                view?.navigateTo(VIEW.LOCATION)
+            } else {
+                view?.toast("Try again-login Failed: ${task.exception?.message}")
+            }
+            view?.hideProgress()
+        }
+    }
+
+    fun doLogIn(email: String, password: String) {
+        view?.showProgress()
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(view!!) { task ->
+            if (task.isSuccessful) {
+                if (app.firestore != null) {
+                    app.firestore!!.fetchLocation {
+                        view?.hideProgress()
+                        view?.navigateTo(VIEW.LOCATION)
+                    }
+                } else {
+                    view?.hideProgress()
+                    view?.navigateTo(VIEW.LOCATION)
+                }
+            } else {
+                view?.hideProgress()
+                view?.toast("Logging Up Failed: ${task.exception?.message}")
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/org/wit/emergencyescape/views/login/LoginView.kt
+++ app/src/main/java/org/wit/emergencyescape/views/login/LoginView.kt
@@ -1,3 +1,60 @@
 package org.wit.emergencyescape.views.login
 
+import android.os.Bundle
+import android.provider.Settings.Global.getString
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.*
+import org.wit.emergencyescape.R
+import org.wit.emergencyescape.views.BaseView
+import org.wit.emergencyescape.views.VIEW
+
+
+class LoginView : BaseView(), AnkoLogger {
+
+    lateinit var presenter : LoginPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        presenter = initPresenter (LoginPresenter(this)) as LoginPresenter
+
+        toolbarLogIn.title = getString(R.string.login_title)
+        setSupportActionBar(toolbarLogIn)
+
+        progressBar.visibility = View.GONE
+
+        btnLogIn.setOnClickListener() {
+            val email = logInEmail.text.toString()
+            val password = logInPassword.text.toString()
+            presenter.doLogIn(email, password)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+
+            R.id.reg -> {
+                navigateTo(VIEW.REGISTER)
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_login, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun showProgress() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    override fun hideProgress() {
+        progressBar.visibility = View.GONE
+    }
 }
