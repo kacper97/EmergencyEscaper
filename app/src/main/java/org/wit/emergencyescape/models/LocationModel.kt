cfa337a7c7a335f91bb2a705e05792f570495f1b
unknown <kacperwoloszyn4@gmail.com>
2019-01-07 13:02:33 +0000

    adding User / Location Models

--- app/src/main/java/org/wit/emergencyescape/models/LocationModel.kt
+++ app/src/main/java/org/wit/emergencyescape/models/LocationModel.kt
@@ -1,2 +1,11 @@
 package org.wit.emergencyescape.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class LocationModel(var id: Long=0,
+                         var fbID : String = "",
+                         var lat: Double = 0.0,
+                         var lng: Double = 0.0,
+                         var zoom: Float = 0f) : Parcelable
