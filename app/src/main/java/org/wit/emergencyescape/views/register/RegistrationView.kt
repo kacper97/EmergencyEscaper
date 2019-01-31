75db0147ad19281bc58b34661685c9faf683b4c3
unknown <kacperwoloszyn4@gmail.com>
2019-01-31 02:42:19 +0000

    According to plan splash done

--- app/src/main/java/org/wit/emergencyescape/views/register/RegistrationView.kt
+++ app/src/main/java/org/wit/emergencyescape/views/register/RegistrationView.kt
@@ -10,6 +10,7 @@ import org.jetbrains.anko.toast
 import org.wit.emergencyescape.R
 import org.wit.emergencyescape.views.BaseView
 import org.wit.emergencyescape.views.VIEW
+import org.wit.emergencyescape.views.login.LoginView
 
 class RegistrationView : BaseView(), AnkoLogger {
 
@@ -21,11 +22,10 @@ class RegistrationView : BaseView(), AnkoLogger {
 
         toolbarRegister.title = getString(R.string.reg_title)
         setSupportActionBar(toolbarRegister)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         presenter = initPresenter (RegistrationPresenter(this)) as RegistrationPresenter
 
-        button_register.setOnClickListener() {
+        button_register.setOnClickListener {
 
             val email = regEmail.text.toString()
             val password = regPassword.text.toString()
@@ -35,19 +35,4 @@ class RegistrationView : BaseView(), AnkoLogger {
 
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-
-            R.id.reg_cancel -> {
-                navigateTo(VIEW.LOGIN)
-                finish()
-            }
-
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        return super.onCreateOptionsMenu(menu)
-    }
 }
