5a0bc972732d528b33a3655cc590988d4c562116
unknown <kacperwoloszyn4@gmail.com>
2019-02-08 00:30:34 +0000

    Before location is displayed properly, map works in great detail

--- app/src/main/java/org/wit/emergencyescape/activities/SplashScreen.kt
+++ app/src/main/java/org/wit/emergencyescape/activities/SplashScreen.kt
@@ -10,7 +10,7 @@ import org.wit.emergencyescape.views.login.LoginView
 
 class SplashActivity : AppCompatActivity() {
     private var mDelayHandler: Handler? = null
-    private val SPLASH_DELAY: Long = 3000 //3 seconds
+    private val SPLASH_DELAY: Long = 1500 //1,5 seconds
 
     internal val mRunnable: Runnable = Runnable {
         if (!isFinishing) {
