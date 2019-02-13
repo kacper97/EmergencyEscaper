7918f480296858e88f794b5dd85cd8a0350dca51
unknown <kacperwoloszyn4@gmail.com>
2019-02-13 12:45:55 +0000

    Update password

--- app/src/main/java/org/wit/emergencyescape/SettingsPresenter.kt
+++ app/src/main/java/org/wit/emergencyescape/SettingsPresenter.kt
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_registration.*
 import org.jetbrains.anko.toast
 import org.wit.emergencyescape.views.BasePresenter
 import org.wit.emergencyescape.views.BaseView
@@ -11,13 +12,14 @@ import org.wit.emergencyescape.views.BaseView
 class SettingsPresenter(view: BaseView) : BasePresenter(view) {
 
     var auth: FirebaseAuth = FirebaseAuth.getInstance()
-
     private val user =  FirebaseAuth.getInstance().currentUser
+
     fun doSaveSettings(settings_email: String, settings_password: String) {
 
         if (settings_email.isEmpty() || settings_password.isEmpty()) {
             view?.toast(R.string.hintEmailPassword)
-        } else user!!.updatePassword(settings_password).addOnCompleteListener{
+        } else user!!.updatePassword(settings_password).addOnCompleteListener {
+            user.updateEmail(settings_email)
             view?.toast(R.string.updatedPassword)
             view?.setResult(AppCompatActivity.RESULT_OK)
             view?.finish()
