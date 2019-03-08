ae7b7a3e81029d0a5bbf7e733385652b5a3fa681
unknown <kacperwoloszyn4@gmail.com>
2019-03-08 06:31:00 +0000

    Adding Sound Receiver in arduino

---
+++ SoundReceiverBuzzer/SoundReceiverBuzzer.ino
@@ -0,0 +1,22 @@
+
+int buzzerPin=9; //buzzer is on pin 9
+int sensorPin=7; // sensor 
+boolean val =0; // boolean set to off
+
+void setup(){
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin (9600);
+}
+  
+void loop (){
+  val =digitalRead(sensorPin);
+  Serial.println (val);
+  // when the sensor detects a signal above the threshold value, Buzzer sounds
+  if (val==HIGH) {
+    digitalWrite(buzzerPin, HIGH);
+  }
+  else {
+    digitalWrite(buzzerPin, LOW);
+  }
+}
